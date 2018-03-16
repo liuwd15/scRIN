420ede6de6ad08990e9eb59852b10ca20d94c6d3
liuwd15 <1045561474@qq.com>
2018-03-16 19:44:59 +0800

    Plots need to be improved.

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# Test
-Scripts for mRNA integrity measurement.
+﻿# Documentation
+This program calculates KS, TIN and some other metrics for each transcript in BED file.
+
+## Requirements:
+You have to install or update some python packages before running this program.
+python >= 2.7
+matplotlib >= 2.2.0
+pysam >= 0.9
+RseQC >= 2.6.4
+numpy any version
+scipy any version
+
+## Installation
+This program is a python script and works in unix-like operating systems.
+You can download it from [Github](https://github.com/liuwd15/Test/blob/master/ks_tin.py).
+> wget https://github.com/liuwd15/Test/blob/master/ks_tin.py
+
+Then you can run it with python.
+> python ks_tin.py
+
+Or you can make it executable and move it to your PATH.
+> chmod +x ks_tin.py
+> mv ks_tin.py ~/bin #"~/bin" can be replaced with other path in you PATH.
+> ks_tin.py
+
+## Usage
+The required input files include:
+
+* Sorted and indexed .bam file(s). You can use samtools to sort and index a .bam file.
+> samtools sort -o example_sorted.bam example.bam
+> samtools index example_sorted.bam
+
+* Reference .bed file containing a list of gene models. Refseq transcripts are recommended.
+
+The simplest usage is:
+> ks_tin.py -r example_refseq.bed -i example_sorted.bam
+
+To reduce the effect of alternative splicing in single cells, use **-e** option to calculate TIN on exons.
+> ks_tin.py -r example_refseq.bed -i example_sorted.bam -e
+
+The gene models with low expression are filted out. The default threshold is average read coverage (read length * mapped read number / gene model length) > 1, you can change it with **-d** option.
+> ks_tin.py -r example_refseq.bed -i example_sorted.bam -d 2
+
+It is also recommended that many .bam files should be processed together.
+You can input comma-separated .bam files like this.
+>ks_tin.py -r example_refseq.bed -i example1_sorted.bam,example2_sorted.bam,example3_sorted.bam
+
+You can also create a text file containing the path of all .bam files like this.
+> cat samples.txt
+> ~/data/examples/example1/STAR_out/example1_sorted.bam
+> ~/data/examples/example2/STAR_out/example2_sorted.bam
+> ~/data/examples/example3/STAR_out/example3_sorted.bam
+
+Then input the text file.
+> ks_tin.py -r example_refseq.bed -i samples.txt
+
+## Output
+For each sample(.bam file), a .xls file containing some mRNA integrity metrics of each gene model will be created in the **same directory as .bam file**.
+For all samples, a .xls file containing 2 overall mRNA integrity metrics (KS and TIN) and a .pdf file plotting overall mRNA integrity of each sample will be created in the **current directory**.
+For all gene models, a .xls file containing 2 overall mRNA integrity metrics (KS and TIN) and a .pdf file plotting overall mRNA integrity of each gene model will be created in the **current directory**.
--- ks_tin.py
+++ ks_tin.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 '''-------------------------------------------------------------------------------------------------
-This program calculates KS, TIN and some other metrics for each transcript or exon in BED file.
+This program calculates KS, TIN and some other metrics for each transcript in BED file.
+
+Requirements:
+python >= 2.7
+matplotlib >= 2.2.0
+pysam >= 0.9
+RseQC >= 2.6.4
 
 Copyright (C) 2018 Wendao Liu
 Copyright (C) 2016 original author Liguo Wang
@@ -12,8 +18,12 @@ of the License, or (at your option) any later version.
 -------------------------------------------------------------------------------------------------'''
 import sys,os
 import math
-import numpy
 import pysam
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+plt.style.use('seaborn')
 from optparse import OptionParser
 from qcmodule import getBamFiles
 from time import strftime
@@ -27,8 +37,23 @@ def printlog (message):
     '''
     message="@ " + strftime("%Y-%m-%d %H:%M:%S") + ": " + message
     print >>sys.stderr,message
+    
+def count_line(file):
+    '''
+    file: string, path of the file.
+    Count the number of lines in the bedfile.
+    '''
+    count = 0  
+    thefile=open(file,'rb')  
+    while True:  
+        buffer=thefile.read(1024*8192)  
+        if not buffer:  
+            break  
+        count+=buffer.count('\n')  
+    thefile.close()  
+    return count 
 
-def genomic_positions(refbed,exon):
+def genomic_positions(refbed):
     '''
     refbed: string, reference BED file including all genes.
     exon: bool, determining whether to calculate metrics on exons rather than transcripts.
@@ -44,84 +69,75 @@ def genomic_positions(refbed,exon):
             #Parse fields from BED file. 
             fields = line.split()
             chrom = fields[0]
-            chromStart = int( fields[1] )
-            chromEnd = int( fields[2] )
+            chrom_start = int( fields[1] )
             name = fields[3]
-            strand = fields[5]
-            exonCount = int(fields[9])               
-            exonStart = map( int, fields[11].rstrip( ',\n' ).split( ',' ) )
-            exonStart = map((lambda x: x + chromStart ), exonStart)
-            exonEnd = map( int, fields[10].rstrip( ',\n' ).split( ',' ) )
-            exonEnd = map((lambda x, y: x + y ), exonStart, exonEnd)
+            strand = fields[5]               
+            exon_starts = map( int, fields[11].rstrip( ',\n' ).split( ',' ) )
+            exon_starts = map((lambda x: x + chrom_start ), exon_starts)
+            exon_ends = map( int, fields[10].rstrip( ',\n' ).split( ',' ) )
+            exon_ends = map((lambda x, y: x + y ), exon_starts, exon_ends)
         except:
             print >>sys.stderr,"[NOTE:input bed must be 12-column] skipped this line: " + line,
             continue
             
-        #Return genomic positions of each nucleotide in exons.
-        if exon:
-            for i in range(exonCount):
-                yield (name + '.' + str(i+1), chrom, exonStart[i], exonEnd[i], list(range(exonStart[i], exonEnd[i])), strand)
         #Return genomic positions of each nucleotide in transcripts.
         else:
-            nucleotide_positions=[]
-            for start,end in zip(exonStart,exonEnd):
-                nucleotide_positions.extend(range(start,end))        
-            yield (name, chrom, chromStart, chromEnd, nucleotide_positions, strand)
+            yield (name, chrom, exon_starts, exon_ends, strand)
         
 
-def calculate_coverage(samfile, chrom, positions):
+def calculate_coverage(samfile, chrom, exon_starts, exon_ends):
     '''
     samfile: pysam.Samfile, input BAM file.
     chrom: string, index of chromosome.
     positions: list of integers, genomic positions of nucleotides.
     Return coverage on positions, a list of integers.
            coverage rate, a float number.
-           peak, an integer.
     '''
-    start = positions[0]            #Starting position of transcript/exon.
-    end = positions[-1]             #Ending position of transcript/exon.
-    length = len(positions)         #Length of transcript/exon.
-    coverage = [0] * length         #Coverage of transcript/exon.
-    covered_nucleotide = 0          #Number of covered nucleotides.
-    peak = 0                        #Number of read peaks.
-    i = 0
-    
-    #Calculate coverage of transcript/exon.
-    for pileupcolumn in samfile.pileup(chrom, start, end):
-        try:
-            i = positions.index(pileupcolumn.pos,i)
-            coverage[i] = pileupcolumn.n
-            covered_nucleotide += 1
-        except:
-            continue
+    #Calculate coverage of exons.
+    if options.exon:
+        exon_coverages = []
+        covered_length = 0.0
+        total_length = 0
+        for (start, end) in zip(exon_starts, exon_ends):
+            exon_coverage = np.array(samfile.count_coverage(chrom, start, end)).sum(axis=0)
+            exon_coverages.append(exon_coverage)
+            total_length += exon_coverage.size
+            covered_length += sum(exon_coverage > 0)
+        #Percentage of covered nucleotides.
+        coverage_rate = covered_length / total_length
+        return exon_coverages,coverage_rate
         
-    coverage_rate = float(covered_nucleotide) / length       #Percentage of covered nucleotides.
-    #Calculate number of read peaks.
-    if coverage[0] > 0:
-        peak += 1
-    for i in range(1,length):
-        if coverage[i] > 0 and coverage[i-1] == 0:
-            peak += 1
-    return coverage,coverage_rate,peak
+    #Calculate coverage of transcript.
+    coverage = np.array([]) 
+    for (start, end) in zip(exon_starts, exon_ends):
+        exon_coverage = np.array(samfile.count_coverage(chrom, start, end)).sum(axis=0)
+        coverage = np.append(coverage, exon_coverage)
+    #Percentage of covered nucleotides.    
+    coverage_rate = float(sum(coverage > 0)) /  coverage.size      
+    return coverage,coverage_rate
     
 def calculate_ks(coverage,read_length):
     '''
-    coverage: list of integers, coverage on nucleotides.
+    coverage: array of integers, coverage on nucleotides.
     read_length: integer, the length of reads.
     Return KS and corresponding P value.
     '''
-    length = len(coverage)
-    depth = sum(coverage)            #depth = read number * read length
-    #if length == 0 or depth == 0:
-    #    return 0,1
+    #Calculate KS of transcript.
+    if options.exon:
+        coverage = np.concatenate(coverage)
+        
+    length = coverage.size
+    depth = np.sum(coverage)            #depth = read number * read length
     
     max_difference = 0.0 
-    for i in range(1,length):
+    cumulative_coverage = 0.0
+    for i in range(length):
         #coverage here becomes empirical CDF of coverage.
-        coverage[i] += coverage[i-1]
-        if abs(max_difference) < abs(float(coverage[i])/depth - float(i+1)/length):
-            max_difference = float(coverage[i])/depth - float(i+1)/length
-            
+        cumulative_coverage += coverage[i]
+        if abs(max_difference) < abs(cumulative_coverage/depth - float(i+1)/length):
+            max_difference = cumulative_coverage/depth - float(i+1)/length
+    
+    #Trapezoid uniform coverage.        
     #coverage_expect = []
     #if(read_length >= length):
     #    coverage_expect = [1/length] * int(length)
@@ -136,7 +152,7 @@ def calculate_ks(coverage,read_length):
     #    if abs(d) < abs(coverage[x]/depth - sum(coverage_expect[0:x])):
     #        d = coverage[x]/depth - sum(coverage_expect[0:x])
     
-    ks = math.sqrt(depth) * max_difference
+    ks = max_difference #* math.sqrt(depth)
     p = smirnov(int(depth/read_length),abs(max_difference))
     return ks,p
 
@@ -145,11 +161,22 @@ def calculate_tin(coverage):
     coverage: list of integers, coverage on nucleotides.
     Return TIN value.
     '''
-    length = len(coverage)
-    depth = sum(coverage)
-    #if length == 0 or depth == 0:
-    #    return 0
-        
+    #Calculate mean TIN of exons.
+    if options.exon:
+        length = np.concatenate(coverage).size
+        mean_tin = 0.0
+        for coverage_i in coverage:            
+            depth_i = np.sum(coverage_i)
+            entropy = 0.0
+            for i in coverage_i: 
+                if i > 0:
+                    entropy += (float(i)/depth_i) * math.log(float(i)/depth_i)    
+            mean_tin += 100*(math.exp(-entropy)) / length
+        return mean_tin
+    
+    #Calculate TIN of transcript.
+    length = coverage.size
+    depth = np.sum(coverage)      
     entropy = 0.0
     for i in coverage:
         if i > 0:
@@ -157,13 +184,158 @@ def calculate_tin(coverage):
     tin = 100*(math.exp(-entropy)) / length
     return tin
 
+def mean_coverage(coverage):
+    '''
+    Calculate the mean coverage on a trancript.
+    '''
+    if options.exon:
+        return np.mean(np.concatenate(coverage))
+    
+    return np.mean(coverage)
+
+def coverage_size(coverage):
+    '''
+    Calculate the length of a transcript.
+    '''
+    if options.exon:
+        return np.concatenate(coverage).size
+    
+    return coverage.size
+
+def process_one_bamfile(file, processed_file):
+    '''
+    file: string, path of bamfile to be processed.
+    processed_file: int, number of processed files.
+    Write a metrics file in the same directory as bamfile.
+    '''
+    print >>sys.stderr, "\t" + file         
+    printlog("Processing " + file)
+    samfile = pysam.Samfile(file, "rb")
+    #Details of each transcript/exon.
+    OUT = open(file.replace('bam','') + 'metrics.xls','w')
+    print >>OUT, "\t".join(["geneID","chrom", "length","average_coverage","coverage_rate","tin","ks","p"])
+       
+    processed_transcript = 0            #number of processed transcript/exon.
+    read_length = samfile.head(1).next().infer_query_length()
+    
+    #Process each transcript/exon.
+    for name, chrom, exon_starts, exon_ends, strand in genomic_positions(options.ref_bed):
+        
+        coverage,rate = calculate_coverage(samfile, chrom, exon_starts, exon_ends)
+        
+        #Omit transcripts/exons with average depth.
+        if mean_coverage(coverage) < options.minimum_average_depth:
+            ks_array[processed_file][processed_transcript] = 0
+            tin_array[processed_file][processed_transcript] = 0
+            print >>OUT, '\t'.join([str(i) for i in (name, chrom, coverage_size(coverage), mean_coverage(coverage), rate,  0, 0, 1)])
+        
+        #Process transcripts/exons above minimum average depth.            
+        else:
+            ks,p = calculate_ks(coverage,read_length)
+            tin = calculate_tin(coverage)
+            #This step make sure that 3' bias results in positive KS and 5' bias results in negative KS.
+            if strand == '+':
+                ks = -ks
+            ks_array[processed_file][processed_transcript] = ks
+            tin_array[processed_file][processed_transcript] = tin
+            print >>OUT, '\t'.join([str(i) for i in (name, chrom, coverage_size(coverage), mean_coverage(coverage), rate,  tin, ks,  p)])
+        processed_transcript += 1
+        print >>sys.stderr, " %d/%d transcripts finished\r" % (processed_transcript, transcript_number),
+    
+    OUT.close()   
+    samfile.close()
+
+def process_bamfiles(files):
+    '''
+    files: list of string, bamfiles to be processed.
+    Write a sample summary file in the current directory.
+    '''
+    #Summary file of all samples.
+    SUM_SAMPLE = open('summary_sample.xls','w')
+    print >>SUM_SAMPLE, "\t".join(['Bam_file','ks(mean)','ks(stdev)','tin(median)','tin(stdev)'])
+    
+    sample_names = []
+    sample_median_tins = []
+    sample_tin_stds = []
+    sample_mean_kss = []
+
+    #Process each bamfile.
+    processed_file = 0
+    for file in bamfiles:
+        process_one_bamfile(file, processed_file)
+        #Only sum transcripts with high expression for each sample.
+        high_expression_transcripts = tin_array[processed_file] > 0
+        print >>SUM_SAMPLE, "\t".join( [str(i) for i in (os.path.basename(file),\
+                                        np.mean(ks_array[processed_file,high_expression_transcripts]),\
+                                        np.std(ks_array[processed_file,high_expression_transcripts]),\
+                                        np.median(ks_array[processed_file,high_expression_transcripts]),\
+                                        np.std(ks_array[processed_file,high_expression_transcripts]))])
+        sample_names.append(file.split('/')[-1][:-4])
+        sample_median_tins.append(np.median(tin_array[processed_file,high_expression_transcripts]))
+        sample_tin_stds.append(np.std(tin_array[processed_file,high_expression_transcripts]))
+        sample_mean_kss.append(np.mean(ks_array[processed_file,high_expression_transcripts]))
+        processed_file += 1
+    
+    SUM_SAMPLE.close()
+    #Plot the median TIN of each sample.
+    sample_number = len(sample_names)
+    index = np.arange(sample_number)
+    if sample_number > 50:
+        plt.figure(figsize = [sample_number/6.5, sample_number/5])
+    plt.bar(index, sample_median_tins, yerr=sample_tin_stds, xerr=[sample_mean_kss,[0]*sample_number])
+    plt.ylabel('nonuniform coverage <-- median TIN --> uniform coverage')    
+    plt.xticks(index, sample_names, rotation=90, fontsize=5)   
+    plt.title('median TIN of samples')
+    plt.savefig('sample_median_TIN.pdf')
+    plt.clf()
+    
+def summary_transcript():
+    '''
+    Write a transcript summary file in the current directory.
+    '''
+    #Summary file of high expression transcripts.
+    SUM_TRANSCRIPT = open('summary_transcript.xls','w')
+    print >>SUM_TRANSCRIPT, "\t".join(["geneID","chrom",'ks(mean)','ks(stdev)','tin(median)','tin(stdev)'])
+    processed_transcript = 0
+    tins = []
+    kss = [] 
+    for line in open(options.ref_bed,'r'):
+        if line.startswith(('#','track','browser')):continue  
+        #Parse fields from BED file. 
+        fields = line.split()
+        chrom = fields[0]
+        name = fields[3]
+        #Only output transcript with high expression in more than 50% samples.
+        if np.mean(tin_array[:,processed_transcript]>0) > 0.5:
+            print >>SUM_TRANSCRIPT, "\t".join( [str(i) for i in (name, chrom,\
+                                                np.mean(ks_array[:,processed_transcript]),\
+                                                np.std(ks_array[:,processed_transcript]),\
+                                                np.median(tin_array[:,processed_transcript]),\
+                                                np.std(tin_array[:,processed_transcript]))])
+            tins.append(np.median(tin_array[:,processed_transcript]))
+            kss.append(np.mean(ks_array[:,processed_transcript]))
+        processed_transcript += 1
+        
+    SUM_TRANSCRIPT.close()
+    #Plot the mean KS and median TIN of transcripts.
+    f = plt.figure()
+    ax = f.add_subplot(111)
+    plt.scatter(kss, tins, s=3)
+    plt.ylabel('nonuniform coverage <-- median TIN --> uniform coverage')
+    plt.xlabel("5' bias <-- mean KS --> 3' bias")
+    plt.title('KS and TIN of transcripts')
+    cor = round(np.corrcoef(kss, tins)[0,1],2)
+    plt.text(0.1, 0.9,"r="+str(cor), ha='center', va='center', transform=ax.transAxes)
+    plt.savefig('transcript_KS_and_TIN.pdf')
+    plt.clf()
+
 #Set options.
 usage="%prog [options]" + '\n' + __doc__ + '\n'
 parser = OptionParser(usage)
 parser.add_option("-i","--input",action="store",type="string",dest="input_files",help='Input BAM file(s). "-i" takes these input: 1) a single BAM file. 2) "," separated BAM files (no spaces allowed). 3) directory containing one or more bam files. 4) plain text file containing the path of one or more bam files (Each row is a BAM file path). All BAM files should be sorted and indexed using samtools. [required]')
 parser.add_option("-r","--refbed",action="store",type="string",dest="ref_bed",help='Reference gene model in BED format. Must be strandard 12-column BED file. [required]')
-parser.add_option("-e","--exon",action="store_true",dest="exon",help="Calculate KS and TIN on exons rather than transcripts.")
 parser.add_option("-d","--minDepth",action="store",type="int",dest="minimum_average_depth",default=1,help="Minimum average depth on each transcript/exon. default=%default")
+parser.add_option("-e","--exon",action="store_true",dest="exon",help="Calculate adjusted TIN on exons to reduce the effect of alternative splicing. This will result in bigger TIN.")
 (options,args)=parser.parse_args()
 
 if not (options.input_files and options.ref_bed):
@@ -178,55 +350,18 @@ if not os.path.exists(options.ref_bed):
 #Read BAM files.    
 printlog("Get BAM file ...")
 bamfiles = getBamFiles.get_bam_files(options.input_files)
+transcript_number = count_line(options.ref_bed)
+bamfile_number = len(bamfiles)
     
-if len(bamfiles) <= 0:
+if bamfile_number <= 0:
     print >>sys.stderr, "No BAM file found, exit."
     sys.exit(0)
 else:
     print >>sys.stderr, "Total %d BAM file(s):" % len(bamfiles)
         
-#Process each BAM file.
-for f in bamfiles:
-    print >>sys.stderr, "\t" + f         
-    printlog("Processing " + f)
-    samfile = pysam.Samfile(f, "rb")
+#Matrix of ks and tin of all transcript in all samples.
+ks_array = np.zeros((bamfile_number, transcript_number))
+tin_array = np.zeros((bamfile_number, transcript_number))
 
-    #Summary file of a sample.
-    SUM = open(os.path.basename(f).replace('bam','') + 'summary.txt','w')
-    print >>SUM, "\t".join(['Bam_file','ks(mean)','ks(stdev)','tin(median)','tin(stdev)'])
-    
-    #Details of each transcript/exon.
-    OUT = open(os.path.basename(f).replace('bam','') + 'metrics.txt','w')
-    print >>OUT, "\t".join(["geneID","chrom", "length","average_depth","coverage_rate","peak","tin","ks","p"])
-    
-    all_ks = []
-    all_tin = []    
-    finish = 0            #number of processed transcript/exon.
-    read_length = samfile.head(1).next().infer_query_length()
-    gene_object = "exons" if options.exon else "transcripts"
-    
-    #Process each transcript/exon.
-    for name, chrom, chromStart, chromEnd, positions, strand in genomic_positions(options.ref_bed,options.exon):
-        finish += 1
-        length = len(positions) 
-        coverage,rate,peak = calculate_coverage(samfile, chrom,sorted(positions))
-        
-        #Omit transcripts/exons with average depth.
-        if numpy.mean(coverage) < options.minimum_average_depth:
-            print >>OUT, '\t'.join([str(i) for i in (name, chrom, length, numpy.mean(coverage), rate, peak, 0, 0, 1)])
-        #Process transcripts/exons above minimum average depth.
-        else:
-            ks,p = calculate_ks(coverage,read_length)
-            tin = calculate_tin(coverage)
-            #This step make sure that 3' bias results in positive KS and 5' bias results in negative KS.
-            if strand == '+':
-                ks = -ks
-            all_ks.append(ks)
-            all_tin.append(tin)
-            print >>OUT, '\t'.join([str(i) for i in (name, chrom, length, numpy.mean(coverage), rate, peak, tin, ks,  p)])
-        print >>sys.stderr, " %d %s finished\r" % (finish, gene_object),
-    
-    print >>SUM, "\t".join( [str(i) for i in (os.path.basename(f), numpy.mean(all_ks), numpy.std(all_ks), numpy.median(all_tin), numpy.std(all_tin))])
-    OUT.close()
-    SUM.close()
-    samfile.close()
+process_bamfiles(bamfiles)
+summary_transcript()
